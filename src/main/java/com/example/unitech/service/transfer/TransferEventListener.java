729bc5b403d48832060edb8efcbc84d8fb91ab0f
Murad Aliyev <murad04@list.ru>
2023-12-02 23:23:42 +0400

    cleanup

--- src/main/java/com/example/unitech/service/transfer/TransferEventListener.java
+++ src/main/java/com/example/unitech/service/transfer/TransferEventListener.java
@@ -2,14 +2,14 @@ package com.example.unitech.service.transfer;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
 public class TransferEventListener {
 
-    @EventListener
+    @TransactionalEventListener
     public void listenTransfer(TransferCompletedEvent source) {
         log.info("Transfer completed with status = " + source.getTransfer().getStatus());
         // additional logic
